6cbc8efe9a69563b42f1e270f1e1f4c0f2ecc6f7
jpersson <shua74@charter.net>
2018-09-23 07:12:26 -0400

    Updated README.md and index.php.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Joshua Persson CSCIE-15 Project 2</title>
+
+        <h1> WSP Geospatial Project Tracker</h1>
+
+</head>
+
+<body>
+</body>
+
+</html>
