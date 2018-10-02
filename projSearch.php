94438081ef73b8b3202cb1ac2fc451228bd2b3c3
jpersson <shua74@charter.net>
2018-10-01 20:58:46 -0400

    Added comments and updated README.md.

--- projSearch.php
+++ projSearch.php
@@ -1,21 +1,31 @@
 <?php
 
+//Checks to see if Session has started, starts if it has not
+
 if (!isset($_SESSION)) {
     session_start();
 }
 
+//Checks the search submission
+
 if (!empty($_GET['search'])) {
     $projSearch = $_GET['projSearch'];
+
+//Gets existing data from JSON
 
     $projectsJSON = file_get_contents('projects.json');
 
     $data = json_decode($projectsJSON, true);
+
+//Loops through array to search for data
 
     foreach ($data as $projectID => $project) {
         if ($project['ProjectID'] != $projSearch) {
             unset($data[$projectID]);
         }
     }
+
+//Writes data to Session to make available to searchlogic.php
 
     $_SESSION['results'] = [
 
